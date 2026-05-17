d7393d952ca913a99d85225b210001251f48441a
Yameee23 <clement.chabrillanges@etu.u-paris.fr>
2026-05-17 17:16:22 +0200

    Complete app.js and ApiService.js

--- js/ApiService.js
+++ js/ApiService.js
@@ -11,13 +11,14 @@ export class ApiService {
    * @returns {Promise<GameReturn>}
    */
   static async createGame(pseudo, difficulty) {
-    const response = await fetch(`${MEMORY_URL}/`, {
+    const response = await fetch(`${MEMORY_URL}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        // Todo compléter le body
+        name : pseudo,
+        difficulty : difficulty
       })
     });
 
--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 import {DOMManager} from './DOMManager.js';
 import {Game} from './Game.js';
 import {ApiService} from './ApiService.js';
+import {imageCollections} from './ImageCollection.js';
 
 const domManager = new DOMManager();
 const game = new Game();
@@ -8,15 +9,27 @@ const game = new Game();
 
 document.querySelector('.game-form').addEventListener('submit', async function (event) {
   event.preventDefault();
+
+
   // Todo À compléter
+  const pseudo = document.querySelector('#pseudo').value.trim();
+  const difficulty = document.querySelector('#difficulty').value;
+  const collection = document.querySelector('#collection').value;
 
   try {
     // Todo Spécifier les paramètres de createGame()
-    const data = await ApiService.createGame();
+    const data = await ApiService.createGame(pseudo, difficulty,collection);
     console.log('Success:', data, data.id);
-    game.startGame(data.id);
+
+    game.startGame(data.id,difficulty,collection, domManager);
+    document.querySelector('.setup-form').classList.add('hidden')  ;
+    document.querySelector('.game-area').classList.remove('hidden');
   } catch (error) {
     console.error('Error:', error);
     alert(error.message || 'Erreur lors de la création de la partie');
   }
+});
+
+document.querySelector('#abandon').addEventListener('click', () => {
+  game.endGame();
 });
