c1d290f25d6d1b45d6e2d96f5f4523bb7c3c9532
Yameee23 <clement.chabrillanges@etu.u-paris.fr>
2026-05-17 16:55:52 +0200

    nouveau game.js

--- js/Game.js
+++ js/Game.js
@@ -7,21 +7,31 @@ export class Game {
    * @type {number} id identifiant de la partie en cours
    */
   #id;
+  #cmp=0;
+  #ref;
+  #click = null;
+  #pairesRestantes=0;
+
 
   async endGame() {
     // Todo À compléter
 
 
-    const idARemplacer = 1234;
-    const nombreDePairesRestanteARemplacer = 5678;
+
+
+    clearInterval(this.#ref);
+    document.querySelector('.game-area').classList.add('hidden');
+    document.querySelector('.setup-form').classList.remove('hidden');
 
     try {
-      const result = await ApiService.updateGameResult(idARemplacer, nombreDePairesRestanteARemplacer);
+      const result = await ApiService.updateGameResult(this.#id, this.#pairesRestantes);
       console.log('Fin de partie:', result);
     } catch (error) {
       console.error('Error:', error);
       alert(error.message || 'Erreur lors de la fin de la partie');
     }
+
+
 
   }
 
@@ -29,13 +39,57 @@ export class Game {
    * Start a new game.
    * @param {number} id - The game ID.
    */
-  startGame(id) {
+  startGame(id,difficulty,collection,domManager) {
     this.#id = id;
+    this.#cmp = 0;
+    this.#pairesRestantes = difficulty;
 
     // Todo À commpléter
+    const img = imageCollections[collection]
+    const diff = img.slice(0, difficulty);
+    const copie = diff.concat(diff)
+
+    for (let i = copie.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [copie[i], copie[j]] = [copie[j], copie[i]];
+    }
+    domManager.createCards(copie)
+    this.#ref = setInterval(() => {
+      this.#cmp++
+      document.querySelector('#chrono').textContent = this.#cmp
+
+    }, 1000);
+
+
+    document.querySelectorAll('.card').forEach(card => {
+      card.addEventListener('click', () => {
+        if (this.#click == null) {
+          this.#click = card
+          card.classList.add('flip')
+        } else if (card === this.#click) {
+          return;
+        } else {
+          if (this.#click.querySelector('.card-back img').src === card.querySelector('.card-back img').src) {
+            card.classList.add('flip')
+            this.#click = null
+            this.#pairesRestantes--;
+            if (this.#pairesRestantes === 0) this.endGame();
+          } else {
+            card.classList.add('flip')
+            const firstCard = this.#click;
+            this.#click = null;
+            setTimeout(() => {
+              card.classList.remove('flip')
+              firstCard.classList.remove('flip')
+            }, 1000)
+          }
+        }
+      })
+    })
+
+
+    // Todo À compléter
+
 
   }
-
-  // Todo À compléter
-
 }
